0322384a8483064b5b55f493037dc154deb8a8df
junilhwang <junil.h@kakao.com>
2020-11-25 00:45:40 +0900

    feat: UserController 작성 완료

--- step7-router-app/src/controller/UserController.php
+++ step7-router-app/src/controller/UserController.php
@@ -8,7 +8,14 @@ class UserController {
     private Router $router;
 
     function __construct(Router $router) {
-        $this->router = $router;
+        $router->get('/api/user', fn($params) => $this->getUser($params));
     }
 
+    private function getUser($params) {
+        header("Content-Type: application/json");
+        echo json_encode([
+            "id" => "junil",
+            "name" => "황준일",
+        ]);
+    }
 }
