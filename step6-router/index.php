418d5eac14ce87a6c3e6fd98decf2a68147b749d
junilhwang <junil.h@kakao.com>
2020-11-24 23:10:57 +0900

    feat: router 코드 작성 중

---
+++ step6-router/index.php
@@ -0,0 +1,27 @@
+<?php
+    class Router {
+        private static Array $routes = [];
+
+        public static function staticPath() {
+
+        }
+
+        public static function use($uri) {
+
+        }
+
+        public static function get() {
+
+        }
+
+        public static function post() {
+
+        }
+    }
+
+    Router::staticPath('/static');
+
+    Router::get('/', function ($param) {
+
+    });
+
---
+++ step6-router/router.php
@@ -0,0 +1,4 @@
+<?php
+
+// PHP Built-in Server에서는 router.php 사용
+include_once("./index.php");
