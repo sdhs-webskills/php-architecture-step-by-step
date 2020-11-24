c3c0b244a98c3657d0b57fd1d3996f0b2a951eca
junilhwang <junil.h@kakao.com>
2020-11-25 01:22:32 +0900

    feat: Put 제거

--- step7-router-app/src/controller/UserController.php
+++ step7-router-app/src/controller/UserController.php
@@ -10,7 +10,6 @@ class UserController {
     function __construct(Router $router) {
         $router->get('/api/users', fn($params) => $this->getUser($params));
         $router->post('/api/user', fn($params) => $this->setUser($params));
-        $router->put('/api/user/([0-9])+', fn($params) => $this->putUser($params));
     }
 
     private function getUser($params) {
@@ -28,9 +27,5 @@ class UserController {
         ];
         setData($users, "users");
         echo json_encode([ 'success' => true ]);
-    }
-
-    private function putUser($params) {
-        echo json_encode([ 'success' => true, 'put' => true, 'body' => $_POST ]);
     }
 }
