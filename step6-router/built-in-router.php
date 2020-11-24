2516010b738fa3c20ad8963ece21d11731a16863
junilhwang <junil.h@kakao.com>
2020-11-24 23:46:35 +0900

    feat: router 코드 작성 중 (3)

--- step6-router/built-in-router.php
+++ step6-router/built-in-router.php
@@ -1,4 +1,7 @@
 <?php
 
 // PHP Built-in Server에서는 built-in-router.php 사용
+if (preg_match("/^\/static/", str_replace('/step6-router', '', $_SERVER['REQUEST_URI']))) {
+    return false;
+}
 include_once(__DIR__ . "/index.php");
--- step6-router/index.php
+++ step6-router/index.php
@@ -1,34 +1,46 @@
 <?php
-    class Router {
-        private Array $routes = [];
-        private String $baseUri;
+function print_pre ($obj) {
+    echo "<pre>";
+    print_r($obj);
+    echo "</pre>";
+    exit;
+}
+class Router {
+    private Array $routes = [];
+    private String $baseUri;
+    private String $requestUri;
 
-        function Router($baseUri) {
-            $this->baseUri = $baseUri;
-        }
-
-        public function staticPath($path) {
-
-        }
-
-        public function use($uri, $callback) {
-
-        }
-
-        public function get($uri, $callback) {
-
-        }
-
-        public function post($uri, $callback) {
-
-        }
+    function Router($baseUri) {
+        $this->baseUri = $baseUri;
+        $this->requestUri = str_replace($baseUri, "", $_SERVER['REQUEST_URI']);
     }
 
-    $router = new Router('/step6-router');
+    public function use($uri, $callback) {
 
-    $router->staticPath('/static');
+    }
 
-    $router->get('/', function ($param) {
+    public function get($uri, $callback) {
+        $this->routes[] = ["get", $uri, $callback];
+    }
 
-    });
+    public function post($uri, $callback) {
+        $this->routes[] = ["post", $uri, $callback];
+    }
+
+    public function run() {
+        array_filter($this->routes);
+    }
+}
+
+$router = new Router('/step6-router');
+
+$router->get('/', function ($param) {
+
+});
+
+$router->get('/test', function ($param) {
+
+});
+
+$router->run();
 
