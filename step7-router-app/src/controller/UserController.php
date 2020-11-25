bb563acbe69da1e87fbef31db115e705aea17681
junilhwang <junil.h@kakao.com>
2020-11-25 10:18:17 +0900

    feat: getUser 추가

--- step7-router-app/src/controller/UserController.php
+++ step7-router-app/src/controller/UserController.php
@@ -8,14 +8,23 @@ class UserController {
     private Router $router;
 
     function __construct(Router $router) {
-        $router->get('/api/users', fn($params) => $this->getUser($params));
+        $router->get('/api/users', fn($params) => $this->getUsers($params));
+        $router->get('/api/user/([0-9]+)', fn($params) => $this->getUser($params));
         $router->post('/api/user', fn($params) => $this->setUser($params));
     }
 
-    private function getUser($params) {
+    private function getUsers($params) {
         header("Content-Type: application/json");
         $users = fetchData("users");
         echo json_encode($users);
+    }
+
+    private function getUser($params) {
+        [, $userId] = $params;
+        header("Content-Type: application/json");
+        $users = fetchData("users");
+        $user = current(array_filter($users, fn($user) => $user->id == $userId));
+        echo json_encode($user);
     }
 
     private function setUser($params) {
